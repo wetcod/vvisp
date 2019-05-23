91320f893b1db561b6c48552d4ff2a29a698be47
Software Engineering Laboratory <SELab@SELabs-MacBook-Pro.local>
2019-05-23 17:32:05 +0900

    feat: add branch 'debug' & modify debug.js (probably error)

--- packages/vvisp/scripts/debug.js
+++ packages/vvisp/scripts/debug.js
@@ -2,31 +2,46 @@ module.exports = async function(files, options) {
   options = require('./utils/injectConfig')(options);
   debug = require('./debug/debugger');
 
-  const path = require('path');
-  const fs = require('fs-extra');
-  const { getAllFiles, printOrSilent } = require('@haechi-labs/vvisp-utils');
+  const { printOrSilent } = require('@haechi-labs/vvisp-utils');
 
-  fs.ensureDirSync(path.join('./', 'build'));
-  fs.ensureDirSync(path.join('build', 'contracts'));
+  const command = {
+    command: "debug",
+    description:
+      "Interactively debug any transaction on the blockchain (exp)",
+    builder: {
+      _: {
+        type: "string"
+      }
+    },
+    help: {
+      usage: "vvisp debug [<transaction_hash>]",
+      options: [
+        {
+          option: "<transaction_hash>",
+          description: "Transaction ID to use for debugging."
+        }
+      ]
+    },
+    run: function(options, done) {
+      const debugModule = require("debug");
+      const debug = debugModule("scripts:debug");
 
-  if (files.length === 0) {
-    files = getAllFiles(path.join('./', 'contracts'), filePath => {
-      return path.parse(filePath).ext === '.sol';
-    });
-  }
+      const Config = require('./utils/injectConfig')(options);
+      const Environment = require("../environment");
 
-  const output = await debug(files, options);
+      const { CLIDebugger } = require("../debug");
 
-  Object.entries(output.contracts).forEach(
-    ([contractNamePath, contractContent]) => {
-      const contractName = path.parse(contractNamePath).name;
-      contractContent.contractName = contractName;
-      fs.writeJsonSync(
-        path.join('./build/contracts', contractName + '.json'),
-        contractContent,
-        { spaces: '  ' }
-      );
+      Promise.resolve()
+        .then(async () => {
+          const config = Config.detect(options);
+          await Environment.detect(config);
+
+          const txHash = config._[0]; //may be undefined
+          return await new CLIDebugger(config).run(txHash);
+        })
+        .then(interpreter => interpreter.start(done))
+        .catch(done);
     }
-  );
+  };
   printOrSilent('Debugging . . . ', options);
 };
