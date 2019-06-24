61b427c82acbf1df128187dc67f4a5c491bb5c69
wetcod <iscc3208@gmail.com>
2019-06-24 17:48:31 +0900

    fix: fix to use blockchainApiStore

--- packages/vvisp/scripts/test.js
+++ packages/vvisp/scripts/test.js
@@ -1,17 +1,10 @@
 const { printOrSilent } = require('@haechi-labs/vvisp-utils');
-const {
-  exec,
-  execFile,
-  execSync
-} = require('child_process');
-
+const { exec, execFile, execSync, spawn } = require('child_process');
 
 module.exports = async function(files, options) {
-
   let test_command = './node_modules/.bin/truffle test';
   let bc_command = './node_modules/.bin/ganache-cli';
   let port = 8545;
-
   if (options.coverage) {
     test_command = './node_modules/.bin/solidity-coverage';
     bc_command = './node_modules/.bin/testrpc-sc';
@@ -20,27 +13,57 @@ module.exports = async function(files, options) {
 
   files.forEach(file => {
     test_command += ` ${file}`;
-  })
+  });
 
   let ganache_process;
   try {
-    execSync(`nc -z localhost ${port}`) // check port listening...
+    execSync(`nc -z localhost ${port}`); // check port listening...
   } catch {
     ganache_process = execFile(
       bc_command,
-      [
-        `--port=${port}`,
-        '--gasLimit=0xfffffffffff'
-      ],
+      [`--port=${port}`, '--gasLimit=0xfffffffffff'],
       (error, stdout, stderr) => {}
     );
   }
 
-  exec(test_command, (error, stdout, stderr) => {
-    printOrSilent(stdout, options);
+  const cmdarr = test_command.split(' ');
+  const cmd = cmdarr[0];
+  const args = cmdarr.slice(1);
 
+  const child = spawn(cmd, args);
+  child.stdout.on('data', function(data) {
+    printOrSilent(data.toString(), options);
+  });
+
+  child.stderr.on('data', function(data) {
+    printOrSilent(data.toString(), options);
+  });
+
+  child.on('exit', function(code) {
     if (ganache_process) {
       ganache_process.kill();
     }
   });
-}
+
+  // ls    = spawn('ls', ['-lh', '/usr']);
+
+  // ls.stdout.on('data', function (data) {
+  //   console.log('stdout: ' + data.toString());
+  // });
+
+  // ls.stderr.on('data', function (data) {
+  //   console.log('stderr: ' + data.toString());
+  // });
+
+  // ls.on('exit', function (code) {
+  //   console.log('child process exited with code ' + code.toString());
+  // });
+
+  // exec(test_command, (error, stdout, stderr) => {
+  //   printOrSilent(stdout, options);
+
+  //   if (ganache_process) {
+  //     ganache_process.kill();
+  //   }
+  // });
+};
