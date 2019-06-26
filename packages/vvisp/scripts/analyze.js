d09a0f7ac7d5261490ef73b3f54921971999ebd1
henjinic <henjinic@gmail.com>
2019-06-26 12:24:37 +0900

    feat: remove unnessecary logs

--- packages/vvisp/scripts/analyze.js
+++ packages/vvisp/scripts/analyze.js
@@ -2,17 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 const { execSync } = require('child_process');
-const {
-  getAllFiles,
-  printOrSilent
-} = require('@haechi-labs/vvisp-utils');
+const { getAllFiles, printOrSilent } = require('@haechi-labs/vvisp-utils');
 const { STATE_PATH } = require('../config/Constant');
 
 module.exports = async function(files, options) {
   options = require('./utils/injectConfig')(options);
 
   if (options.allContract) {
-    files = getAllFiles('./contracts', (filePath) => {
+    files = getAllFiles('./contracts', filePath => {
       return path.parse(filePath).ext === '.sol';
     });
   }
@@ -30,41 +27,38 @@ module.exports = async function(files, options) {
 };
 
 function analyzeOnChain(options) {
-  const url = `${options.config.network_config.host}:${options.config.network_config.port}`;
+  const url = `${options.config.network_config.host}:${
+    options.config.network_config.port
+  }`;
   const vvispState = JSON.parse(fs.readFileSync(STATE_PATH, 'utf-8'));
 
-  Object.keys(vvispState.contracts)
-    .forEach(contractName => {
-      printOrSilent(chalk.bold(`Contract: ${contractName}`), options);
+  Object.keys(vvispState.contracts).forEach(contractName => {
+    printOrSilent(chalk.bold(`Contract: ${contractName}`), options);
 
-      const address = vvispState.contracts[contractName].address;
-      const command = `docker run --network=host mythril/myth -xa ${address} --rpc ${url}`;
-      const result = execSync(command, { stdio: 'pipe' }).toString();
+    const address = vvispState.contracts[contractName].address;
+    const command = `docker run --network=host mythril/myth -xa ${address} --rpc ${url}`;
+    const result = execSync(command, { stdio: 'pipe' }).toString();
 
-      printOrSilent(result, options);
-    });
+    printOrSilent(result, options);
+  });
 }
 
 function analyzeOffChain(files, options) {
-  files
-    .forEach(file => {
-      printOrSilent(chalk.bold(`File: ${file}`), options);
+  files.forEach(file => {
+    printOrSilent(chalk.bold(`File: ${file}`), options);
 
-      const dirName = path.dirname(path.resolve(file));
-      const baseName = path.basename(file);
+    const dirName = path.dirname(path.resolve(file));
+    const baseName = path.basename(file);
 
-      console.log(`${dirName}`);
-      console.log(`${baseName}`);
+    const command = `docker run -v ${dirName}:/tmp mythril/myth -x tmp/${baseName}`;
+    const result = execSync(command, { stdio: 'pipe' }).toString();
 
-      const command = `docker run -v ${dirName}:/tmp mythril/myth -x tmp/${baseName}`;
-      const result = execSync(command).toString();
-
-      printOrSilent(result, options);
-    });
+    printOrSilent(result, options);
+  });
 }
 
 function printRequirements() {
   console.error('# docker must be installed');
-  console.error('# docker\'s permission must be set properly');
+  console.error("# docker's permission must be set properly");
   console.error('# mythril/myth must be pulled');
 }
